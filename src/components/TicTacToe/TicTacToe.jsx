7de38d8f92c8dbef9d4cb4b90c8d5fa1a69a475f
Homrajshahi <bindashahi473@gmail.com>
2026-03-04 11:00:46 +0545

    updating the code

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -9,7 +9,17 @@ const TicTacToe = () => {
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
   let titleRef = useRef(null);
-  
+  let box1 = useRef(null);
+  let box2 = useRef(null);
+  let box3 = useRef(null);
+  let box4 = useRef(null);
+  let box5 = useRef(null);
+  let box6 = useRef(null);
+  let box7 = useRef(null);
+  let box8 = useRef(null);
+  let box9 = useRef(null);
+
+  let box_array = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
   const toggle = (e, num) => {
     if (lock) {
@@ -25,7 +35,7 @@ const TicTacToe = () => {
       setCount(++count);
     }
     checkWin();
-  } 
+  };
 
   const checkWin = () => {
     if (data[0] == data[1] && data[1] == data[2] && data[2] !== "") {
@@ -45,24 +55,25 @@ const TicTacToe = () => {
     } else if (data[2] == data[4] && data[4] == data[6] && data[6] !== "") {
       won(data[6]);
     }
-  }
+  };
 
   const won = (winner) => {
     setLock(true);
-    if (winner ==="x"){
-        titleRef.current.innerHTML = `Congratulations: <img src = ${cross_icon}> wins`;
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congratulations: <img src = ${cross_icon}> wins`;
+    } else {
+      titleRef.current.innerHTML = `Congratulations: <img src = ${circle_icon}> wins`;
     }
-    else{
-        titleRef.current.innerHTML = `Congratulations: <img src = ${circle_icon}> wins`;
+  };
 
-    }
-  }
-
-  const reset = () =>{
+  const reset = () => {
     setLock(false);
-    data =["","","","","",""];
-    titleRef.current.innerHTML = 'Tic Tac Toe In <span>React </span>'
-  }
+    data = ["", "", "", "", "", ""];
+    titleRef.current.innerHTML = "Tic Tac Toe In <span> React </span>";
+    box_array.map((e) => {
+      e.current.innerHTML = ";";
+    });
+  };
 
   return (
     <div className="container">
@@ -73,65 +84,81 @@ const TicTacToe = () => {
         <div className="row1">
           <div
             className="boxes"
+            ref={box1}
             onClick={(e) => {
-              toggle(e, 0)
+              toggle(e, 0);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box2}
             onClick={(e) => {
-              toggle(e, 1)
+              toggle(e, 1);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box3}
             onClick={(e) => {
-              toggle(e, 2)
+              toggle(e, 2);
             }}
           ></div>
         </div>
         <div className="row2">
           <div
             className="boxes"
+            ref={box4}
             onClick={(e) => {
-              toggle(e, 3)
+              toggle(e, 3);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box5}
             onClick={(e) => {
-              toggle(e, 4)
+              toggle(e, 4);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box6}
             onClick={(e) => {
-              toggle(e, 5)
+              toggle(e, 5);
             }}
           ></div>
         </div>
         <div className="row3">
           <div
             className="boxes"
+            ref={box7}
             onClick={(e) => {
-              toggle(e, 6)
+              toggle(e, 6);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box8}
             onClick={(e) => {
-              toggle(e, 7)
+              toggle(e, 7);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box9}
             onClick={(e) => {
-              toggle(e, 8)
+              toggle(e, 8);
             }}
           ></div>
         </div>
       </div>
-      <button className="reset" onClick={()=>{reset()}}>Reset</button>
+      <button
+        className="reset"
+        onClick={() => {
+          reset();
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 };
